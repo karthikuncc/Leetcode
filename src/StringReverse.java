c2b65065ebfe5e8ad258dcf9d646b2b8e572431f
Karthik G <karthiky7cs1212@gmail.com>
2017-07-15 13:33:15 -0400

    String Reverse

---
+++ src/StringReverse.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		
+		String str;
+		char xyz[];
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		xyz=str.toCharArray();
+		
+		int i=0,j;
+		j=str.length()-1;
+		
+		while(i<j)
+		{
+		 char tmp;
+		 tmp=xyz[i];
+		 xyz[i]=xyz[j];
+		 xyz[j]=tmp;
+		 i++;
+		 j--;
+		}
+		
+		System.out.println(xyz);
+	}
+
+}
+

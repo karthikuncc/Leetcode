619002750b40b78286c129244c15cf9b07bc4274
Karthik G <karthiky7cs1212@gmail.com>
2017-07-15 12:09:48 -0400

    ArrayPairs

---
+++ src/ArrayPairsSum.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class ArrayPairsSum {
+	
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		sc.nextLine(); 
+		int [] numbers = new int[size];
+		
+		for (int i = 0; i < size; i++) {
+		    if (sc.hasNextInt()) {
+		        numbers[i] = sc.nextInt();
+		    } else {
+		        System.out.println("You didn't provide enough numbers");
+		        break;
+		    }
+		}
+		
+		Arrays.sort(numbers);
+		int result=0;
+		
+		for(int i=0;i<numbers.length;i+=2){
+		 result+=numbers[i];	
+		}
+				System.out.println(result);
+			    sc.close();
+		         
+
+	}
+	
+
+}
